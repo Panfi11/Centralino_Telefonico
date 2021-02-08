a0156a042c55c48fad0b38383b145d9d9b9ffa0e
Pier Andrea Frontani <pierandreapaf@gmail.com>
2021-02-08 21:54:29 +0100

    Aggiungere i file di progetto.

---
+++ Centralino_Telefonico/Sim.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centralino_Telefonico
+{
+    public class Sim
+    {
+        private static long UltimoSerialUtilizzato = 0;
+
+        public long Seriale { get; private set; }
+
+        public string NumeroTelefono { get; private set; }
+
+        public double Credito { get; private set; }
+
+        public List<Telefonata> Telefonate { get; private set; } = new List<Telefonata>();
+
+        public Sim(string numeroTelefono, double credito)
+        {
+            NumeroTelefono = NumeroTelefono;
+            Credito = credito;
+            Seriale = GeneraSeriale();
+        }
+        private static long GeneraSeriale()
+        {
+            return +UltimoSerialUtilizzato;
+        }
+
+        public Telefonata Chiama(string numero, DateTime data, int durata)
+        {
+            Telefonata t = new Telefonata(data, durata, numero);
+            Telefonate.Add(t);
+            return t;
+        }
+
+        public int CalcolaMinutiTotali(string numeri)
+        {
+            int somma = 0;
+            foreach (Telefonata t in Telefonate)
+            {
+                somma += t.DurataInSecondi;
+            }
+            return somma / 60;
+        }
+        public string StampaElenchiChiamate()
+        {
+            string s = "";
+            foreach(Telefonata t in Telefonate.OrderBy())
+            {
+                s += t.ToString() + "\n";
+            }
+            return s;
+
+        }
+    }
+}
---
+++ Centralino_Telefonico/Telefonata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centralino_Telefonico
+{
+    public class Telefonata
+    {
+        public DateTime Data { get; private set; }
+
+        public int DurataInSecondi { get; private set; }
+
+        public string NumeroChiamato { get; private set; }
+
+        public Telefonata (DateTime data, int durataSecondi, string numerochiamato)
+        {
+            Data = data;
+            DurataInSecondi = durataSecondi;
+            NumeroChiamato = numerochiamato;
+        }
+
+        public override string ToString()
+        {
+            return ($"chiamato {NumeroChiamato} il {Data} per{DurataInSecondi} secondi");
+        }
+    }
+}
